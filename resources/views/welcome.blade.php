28ec89e41291825b2ab5b7f00b45c029e88ca724
Davidgsr <dgsrfut07@gmail.com>
2023-12-22 11:20:32 -0300

    Forma Inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,30 +4,30 @@
 
 @section('content')
 
-        <h1>Algum título</h1>
-        <img src="/img/banner.jpg" alt="banner">
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um Evento</h1>
 
-        @if (10 > 5)
-            <p>A condição é true</p>
-        @endif
-        <p>{{$nome}}</p>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos Eventos</h2>
+        <p>Veja os eventos dos próximos dias</p>
 
-        @if ($nome == "Pedro")
-            <p>O nome é Pedro</p>
-        @elseif ($nome == "David")
-            <p>O nome é {{$nome}} e ele tem {{$idade}} anos de idade e trabalha como {{$profissao}}</p>
-        @endif
+        <div id="cards-container" class="row">
+            @foreach($events as $event)
+                <div class="card col-md3">
+                    <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                    <div class="card-body">
+                        <p class="card-date">10/09/2020</p>
+                        <h5 class="card-title">{{ $event->title }}</h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn btn-primary">Saber Mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 
-        @for ($i=0; $i < count ($arr); $i++)
-            <p>{{$arr[$i]}}</p>
-        @endfor
-
-        @foreach ($nomes as $nome)
-            <p>{{$nome}}</p>
-        @endforeach
-
-        @php
-            $name = 'João';
-            echo $name;
-        @endphp
 @endsection
