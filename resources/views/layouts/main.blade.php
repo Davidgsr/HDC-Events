da34960de2e447b0a58b6269db0c2cd2739f5746
Davidgsr <dgsrfut07@gmail.com>
2023-12-22 21:34:50 -0300

    Adicionado página de Eventos

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,14 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/css/style.css">
         <!--Fontes do Google-->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
         <!--Bootstrap-->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">       
-
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">     
+        <link rel="stylesheet" href="/css/style.css">
+        
 
         <title>@yield('title')</title>
 
@@ -39,7 +39,16 @@
                 </div>
             </nav>
         </header>
-        @yield('content')
+            <main>
+                <div class="container-fluid">
+                    <div class="row">
+                        @if(session('msg'))
+                            <p class="msg">{{ session('msg') }}</p>
+                        @endif
+                        @yield('content')
+                    </div>
+                </div>
+            </main>
         <footer>
             <p>HDC Events &copy; 2023</p>
         </footer>
